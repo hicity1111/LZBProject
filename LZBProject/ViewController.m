1f991ae80dcbb78f6aa3a3eef63841bd0fd50b40
hicity <>
2019-10-14 10:27:55 +0800

    设置项目访问权限

--- LZBProject/ViewController.m
+++ LZBProject/ViewController.m
@@ -14,16 +14,13 @@
 
 @implementation ViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    
     
     UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(10, 100, 100, 100)];
     [button setBackgroundColor:[UIColor blueColor]];
     [self.view addSubview:button];
-    
-    
     
     XLDLog(@"vdfshjfs");
     XLDLog(@"kScreenWidth==%f  kScreenHeight==%f",kScreenWidth,kScreenHeight);

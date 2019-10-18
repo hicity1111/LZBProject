bacb40843d5218d0bc64518b6bcebfbea7908c68
hicity <>
2019-10-18 16:14:01 +0800

    /*确认文件*

---
+++ LZBProject/Utility/Category/LZBCalendar/TriangleView.h
@@ -0,0 +1,39 @@
+//
+//  TriangleView.h
+//  LZBProject
+//
+//  Created by hicity on 2019/10/14.
+//  Copyright © 2019 hicity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSUInteger, TriangleViewStyle) {
+    triangleViewIsoscelesTop,
+    triangleViewIsoscelesLeft,
+    triangleViewIsoscelesBottom,
+    triangleViewIsoscelesRight,
+};
+
+@interface TriangleView : UIView
+
+
+/**
+ @param color 填充颜色
+ @param style 三角形样式
+ @return TriangleView
+ */
+- (instancetype)initWithColor:(UIColor *)color style:(TriangleViewStyle)style;
+
+/**
+ @param color 填充颜色,
+     style default:triangleViewIsoscelesTop
+ @return TriangleView
+ */
+- (instancetype)initWithColor:(UIColor *)color;
+
+- (void)TriangleView_setColor:(UIColor *)color style:(TriangleViewStyle)style;
+- (void)TriangleView_setStyle:(TriangleViewStyle)style;
+- (void)TriangleView_setColor:(UIColor *)color;
+
+@end
